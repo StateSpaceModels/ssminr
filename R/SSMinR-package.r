b91d43d1f98d643068f95d8f3050fd40625300a0
Anton <anton.camacho@lshtm.ac.uk>
2015-01-23 11:57:21 +0000

    package + data description

--- R/SSMinR-package.r
+++ R/SSMinR-package.r
@@ -20,3 +20,33 @@ NULL
 #' @source \url{http://apps.who.int/gho/data/view.ebola-sitrep.ebola-country-LBR-latest-download?embed=1}
 #' @name ebola_2014
 NULL
+
+#' Time-series of the Ebola epidemic in Liberia until August 2014
+#'
+#' This dataset contains the weekly incidence of confirmed and probable Ebola virus disease (EVD)
+#' cases recorded during the 2014 Ebola outbreak in Liberia. The time-series span the period 2014-07-01 to 2014-08-25.
+#'
+#' \itemize{
+#'   \item \code{date} calendar date of the record
+#'   \item \code{incidence_obs} weekly count incidence of confirmed and probable EVD cases
+#' }
+#'
+#' @format Data frames
+#' @source \url{http://apps.who.int/gho/data/view.ebola-sitrep.ebola-country-LBR-latest-download?embed=1}
+#' @name liberia1
+NULL
+
+#' Time-series of the Ebola epidemic in Liberia until December 2014
+#'
+#' This dataset contains the weekly incidence of confirmed and probable Ebola virus disease (EVD)
+#' cases recorded during the 2014 Ebola outbreak in Liberia. The time-series span the period 2014-07-01 to 2014-12-03.
+#'
+#' \itemize{
+#'   \item \code{date} calendar date of the record
+#'   \item \code{incidence_obs} weekly count incidence of confirmed and probable EVD cases
+#' }
+#'
+#' @format Data frames
+#' @source \url{http://apps.who.int/gho/data/view.ebola-sitrep.ebola-country-LBR-latest-download?embed=1}
+#' @name liberia2
+NULL
