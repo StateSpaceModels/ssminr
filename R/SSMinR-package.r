1e77c999bd7e09d618de731c76a8028a3242cba1
Anton <anton.camacho@lshtm.ac.uk>
2015-01-23 08:40:57 +0000

    update data

---
+++ R/SSMinR-package.r
@@ -0,0 +1,22 @@
+#' SSMinR
+#'
+#' @name SSMinR
+#' @docType package
+NULL
+
+
+#' Time-series of the 2014 Ebola epidemic in Liberia
+#'
+#' Two datasets \code{liberia1} and \code{liberia2} containing the weekly incidence of confirmed and probable Ebola virus disease (EVD)
+#' cases recorded during the 2014 Ebola outbreak in Liberia. The time-series span the period 2014-07-01 to 2014-08-25 for \code{liberia1}
+#' and 2014-12-03 for \code{liberia2}.
+#'
+#' \itemize{
+#'   \item \code{date} calendar date of the record
+#'   \item \code{incidence_obs} weekly count incidence of confirmed and probable EVD cases
+#' }
+#'
+#' @format Data frames
+#' @source \url{http://apps.who.int/gho/data/view.ebola-sitrep.ebola-country-LBR-latest-download?embed=1}
+#' @name ebola_2014
+NULL
