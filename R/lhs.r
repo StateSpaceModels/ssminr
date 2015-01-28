99d5f1db18ebc92ef7b10a15c4796244c25d085f
Anton <anton.camacho@lshtm.ac.uk>
2015-01-28 16:15:43 +0000

    do any SSM block on a LHS

---
+++ R/lhs.r
@@ -0,0 +1,45 @@
+get_ssm_log_like <- function(ssm) {
+	
+	data_frame(log_like=ssm$summary["log_likelihood"], ssm=list(ssm))
+
+}
+
+#'Get max LHS
+#'
+#'Extract the \code{ssm} object with highest log-likelihood from \code{lhs}.
+#' @param lhs a \code{tbl}, as returned by \code{\link{do_lhs}}
+#' @export
+#' @import dplyr
+#' @seealso \code{\link{do_lhs}}
+#' @return \code{ssm} object
+get_max_lhs <- function(lhs) {
+
+	lhs %>% do(get_ssm_log_like(.$ssm)) %>% filter(log_like==max(log_like)) %>% select(ssm) %>% .[[1,1]]
+
+}
+
+#'Perform LHS
+#'
+#'Function to perform any SSM block on a LHS. The LHS is currently a random sample from the prior distribution.
+#' @param  ... additional arguments to be passed to \code{do}
+#' @inheritParams call_ssm
+#' @inheritParams sample_prior
+#' @export
+#' @import dplyr
+#' @seealso \code{\link{get_max_lhs}}
+#' @return a \code{tbl} containing all \code{ssm} outputed by \code{do}.
+do_lhs <- function(ssm, n, do=c("kalman","kmcmc","ksimplex","mif","pmcmc","simplex","simul","smc"), ...) {
+
+	do <- match.arg(do)
+
+	# create lhs directory
+	dir_lhs <- ssm$model_path %>% file.path("lhs")
+
+	dir.create(dir_lhs, showWarnings=FALSE)
+
+	# run LHS
+	ssm %>% sample_prior(n, method="random") %>% dplyr::do(ssm=do.call(do, list(ssm=.$ssm, id=.$id, root=dir_lhs, ...)))
+
+}
+
+
