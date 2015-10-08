1bff33a3920c502265fcd00bc22e0d55d924e8d1
Anton <anton.camacho@lshtm.ac.uk>
2015-10-08 18:14:59 +0200

    fix bug

--- R/observation.r
+++ R/observation.r
@@ -43,6 +43,6 @@ discretized_normal_obs <- function(obs=NULL, state=NULL, reporting, overdispersi
 
 	x <- set_obs_variable(obs, state)
 
-	return(list(name=x$obs, distribution="discretized_normal",mean=sprintf("%s * (%s)", reporting, x$state), sd = sprintf("sqrt(%s * (%s) + %s * pow(%s * (%s),2))", reporting, reporting,  x$state, overdispersion, reporting, x$state)))
+	return(list(name=x$obs, distribution="discretized_normal",mean=sprintf("%s * (%s)", reporting, x$state), sd = sprintf("sqrt(%s * (%s) + %s * pow(%s * (%s),2))", reporting,  x$state, overdispersion, reporting, x$state)))
 
 }
